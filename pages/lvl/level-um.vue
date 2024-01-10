8288f5dea3f58227b66a0c176c7c7f9418e02c4c
frehsegustavo <frehsegustavo@gmail.com>
2024-01-10 15:34:24 -0300

    [add] fase 1

---
+++ pages/lvl/level-um.vue
@@ -0,0 +1,80 @@
+<template>
+	<v-container fluid class="wrapper IBMPlex d-flex flex-column align-center">
+		<!-- Text -->
+		<div class="text-content d-flex flex-column text-center">
+			<h5>1. Level 1</h5>
+			<v-divider class="my-2"></v-divider>
+			<span>Entendeu o jogo? Então, boa sorte :)</span>
+			<v-img
+				alt="Menina folheando uma revista"
+				src="https://64.media.tumblr.com/2b1a727e2a3e5f3bc8628c5328fbcd5b/tumblr_ppq2feUMPf1x6a7yto1_500.gif"
+				height="20vh"
+				class="my-2"
+			/>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>lembrancas-de-outono</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+		</div>
+		<!-- Text -->
+	</v-container>
+</template>
+<script setup>
+// Cabeçalhos da pagina
+useSeoMeta({
+	title: "Retrozule",
+	description: "Site experimental de puzzles",
+})
+useHead({
+	htmlAttrs: {
+		lang: "pt-br",
+	},
+	link: [
+		{
+			rel: "icon",
+			type: "image/ico",
+			href: "/favicon.ico",
+		},
+	],
+})
+</script>
+<style scoped>
+.wrapper {
+	height: 100%;
+	width: 100%;
+	z-index: 45;
+	color: white;
+	background: rgb(46, 38, 75);
+}
+
+.text-content {
+	overflow-y: auto;
+	width: 95%;
+}
+
+p {
+	color: rgb(46, 38, 75);
+}
+</style>
