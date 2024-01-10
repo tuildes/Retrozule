f1359ee5a5dd8ba25ee58ea554a288704336a730
frehsegustavo <frehsegustavo@gmail.com>
2024-01-10 13:51:30 -0300

    [add] Card mobile de erro

---
+++ components/layout/Mobile.vue
@@ -0,0 +1,15 @@
+<template>
+	<v-card theme="dark" class="pa-2" width="60vw" min-width="300px">
+		<v-card-title>Desculpa!</v-card-title>
+		<v-card-text>
+			Mas esse site não foi feito para telas pequenas [mobile]
+			<v-img
+				class="mt-4"
+				alt="Sailor Moon chorando"
+				src="https://i.pinimg.com/originals/40/bb/68/40bb68680f5a12431e1d64e46c685770.gif"
+			/>
+		</v-card-text>
+	</v-card>
+</template>
+<script setup></script>
+<style></style>
